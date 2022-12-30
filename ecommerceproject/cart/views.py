c812c0854e4acfbe4745ff82dc0c91c7133da827
laya <layaaugustine12345@gmail.com>
2022-12-30 15:49:54 +0530

    modi

--- ecommerceproject/cart/views.py
+++ ecommerceproject/cart/views.py
@@ -59,11 +59,11 @@ def cart_remove(request,product_id):
     product = get_object_or_404(Product,id=product_id)
     cart_item = CartItem.objects.get(product=product,cart=cart)
     if cart_item.quantity > 1:
-        cart_item.quantity-=1
+        cart_item.quantity -=1
         cart_item.save()
     else:
         cart_item.delete()
-        return redirect('cart:cart_detail')
+    return redirect('cart:cart_detail')
 
 
 def full_remove(request,product_id):
